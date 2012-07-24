8e6206addb0d49a390f0832e1838517c249a1fe4
Ben Beecher <benbeecher@gmail.com>
2012-07-24 00:13:16 -0400

    changing some stuff around I guess

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 var App = Em.Application.create();
 
+
+Handlebars.registerHelper("section", function() {
+    var firstLetter = this.text.substring(0,1);
+    var rest = this.text.substring(1);
+    return new Handlebars.SafeString("<span id='" + this.text.toLowerCase() + "'>" + firstLetter + "</span>" + rest)
+})
+
 sections = [
     {text: "Projects"},
     {text: "Services"},
